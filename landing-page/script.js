22baf300ce6bd9e79193fa7c12419648eec4aefb
blazobanicevic <blazoban777@gmail.com>
2026-05-14 21:19:09 +0200

    Finished home section for landing page

---
+++ landing-page/script.js
@@ -0,0 +1,6 @@
+//sign up dugme povezujemo sa register-login-page
+const signUpButton = document.querySelector(".sign-up-bttn");
+
+signUpButton.addEventListener('click', () => {
+    window.location.href = "/register-login-page/index.html";
+});
