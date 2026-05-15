93ff0375c3dd15f25d80f003ff17d255d7aedd07
blazobanicevic <blazoban777@gmail.com>
2026-05-15 09:26:48 +0200

    Finished about section

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,6 +1,17 @@
-//sign up dugme povezujemo sa register-login-page
-const signUpButton = document.querySelector(".sign-up-bttn");
+//svako sign up dugme, tj get started dugme povezujemo sa register-login-page
+const signUpButtons = document.querySelectorAll(".sign-up-bttn");
 
-signUpButton.addEventListener('click', () => {
-    window.location.href = "/register-login-page/index.html";
+signUpButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        window.location.href = "/register-login-page/index.html";
+    });
+});
+
+//learn more dugme povezujemo sa about sekcijom
+const learnMoreButton = document.querySelector(".learn-more-bttn");
+
+learnMoreButton.addEventListener('click', () => {
+    document.querySelector("#about").scrollIntoView({
+        behavior: "smooth"
+    });
 });
